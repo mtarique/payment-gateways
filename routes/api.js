fc8e1211b07862a6bc08a2fd8dab501f12f0d449
mtarique <mtarique@outlook.com>
2023-05-15 00:53:48 +0530

    Razorpay SDK and create order API created

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,17 @@ router.get('/status', (req, res) => {
     res.json({status: true, message: "API is running...", data: ""}); 
 }); 
 
+router.post('/create-order', (req, res) => {
+    const Razorpay = require('razorpay'); 
+    let instance = new Razorpay({key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET})
+    var options = {
+        amount: 5000, 
+        currency: "INR", 
+        receipt: "order_rcptid_11"
+    }
+    instance.orders.create(options, (err, order) => {
+        console.log(order)
+    })
+})
+
 module.exports = router;
