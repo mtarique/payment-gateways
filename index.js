f1f96ca01bb212b5db32922886636f5f59f13430
mtarique <mtarique@outlook.com>
2023-05-15 00:19:59 +0530

    Morgan installed

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express'); 
 const dotenv = require('dotenv'); 
 const path = require('path'); 
+const morgan = require('morgan'); 
 const app = express(); 
 
 dotenv.config(); 
 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
+app.use(express.static(path.join(__dirname, '/public'))); 
+app.use(morgan('dev')); 
 
 app.use('/api', require('./routes/api')); 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is listening at ${process.env.PORT}`); 
+    console.log(`Server is listening on ${process.env.PORT}`); 
 })
